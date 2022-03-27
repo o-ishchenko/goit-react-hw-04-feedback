7ba7675504aa42b6fe7b66c5bb05fe126202ba55
o-ishchenko <olya.galaychuk@gmail.com>
2022-03-27 15:36:26 +0300

    remove import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import { useState } from 'react';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Section from './Section';
